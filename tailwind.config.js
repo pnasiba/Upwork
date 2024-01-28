85779c0374c9f698d65cc2932b7a25a3312bc515
pnasiba <pnasiba23.06@gmail.com>
2024-01-29 03:44:02 +0500

    upwork

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      // backgroundImage: {
+      //   "client-bg": "url('./assets/images/find_client-bg-img.png')",
+      // },
+    },
+  },
+  plugins: [],
+};
